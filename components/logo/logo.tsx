5f3f6be089c849dfbc3e406303dbbf527f4377c4
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-28 22:08:26 +0600

    fix: fix logo with classNames

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,17 +1,20 @@
+import { cn } from "@/lib/utils";
 import { House } from "lucide-react";
 import Link from "next/link";
 
-export default function Logo() {
+type PropsType = { className?: string | string[] };
+
+export default function Logo({ className }: PropsType) {
   return (
     <Link
       aria-label='Go to homepage'
       href='/'
-      className='flex items-center gap-2 font-bold text-green-600'
+      className={cn("flex items-center gap-2 font-bold", className)}
     >
       <House size={24} />
       <span className='md:text-lg text-xl xl:text-xl'>
         <strong>
-          <em>RENTREST</em>
+          <strong>RENTREST</strong>
         </strong>
       </span>
     </Link>
