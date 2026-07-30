d365a37bfc1609a9a78a7d0eb7965125f4db9987
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-30 18:23:17 +0600

    secret: define env in config

--- config/index.ts
+++ config/index.ts
@@ -4,4 +4,11 @@ import type { StringValue } from "ms";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-export default {};
+export default {
+  base_url: process.env.API as StringValue,
+  nodemailer_user: process.env.NODEMAILER_USER as StringValue,
+  nodemailer_app_password: process.env.NODEMAILER_APP_PASSWORD as StringValue,
+  nodemailer_service: process.env.NODEMAILER_SERVICE as StringValue,
+  nodemailer_smtp_host: process.env.SMTP_HOST,
+  nodemailer_smtp_port: process.env.SMTP_PORT,
+};
