dcdc11c491f0445696b77318a36b40a98fea5760
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-30 19:40:35 +0600

    fix: api-response fix according api-response Type

--- utils/api-response.ts
+++ utils/api-response.ts
@@ -3,23 +3,23 @@ import type { ApiResponse } from "@/types/response";
 export function successResponse<T>(
   data: T,
   message = "Success",
-  statusCode = 200,
+  status = 200,
 ): ApiResponse<T> {
   return {
     success: true,
     message,
-    statusCode,
+    status,
     data,
   };
 }
 
 export function errorResponse<T = null>(
   message = "Something went wrong",
-  statusCode = 500,
+  status = 500,
 ): ApiResponse<T> {
   return {
     success: false,
     message,
-    statusCode,
+    status,
   };
 }
