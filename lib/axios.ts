7ab7b8333b5150d51c27045b1af8cd90d2b03bcd
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-30 18:44:04 +0600

    chor: create axios base instance

---
+++ lib/axios.ts
@@ -0,0 +1,12 @@
+import config from "@/config";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: config.base_url,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
