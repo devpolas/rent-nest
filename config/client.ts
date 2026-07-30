c02b41b1292ce3a85a81ccbdc726b2defaa710e0
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-31 02:00:59 +0600

    seperate: server config and client config

---
+++ config/client.ts
@@ -0,0 +1,7 @@
+"use client";
+import type { StringValue } from "ms";
+
+export default {
+  app_name: "Rent Nest",
+  base_url: process.env.NEXT_PUBLIC_API as StringValue,
+};
--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
-import path from "path";
+import "server-only";
 import type { StringValue } from "ms";
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
-
 export default {
+  app_name: "Rent Nest",
+  website: process.env.WEBSITE as StringValue,
   base_url: process.env.API as StringValue,
   nodemailer_user: process.env.NODEMAILER_USER as StringValue,
   nodemailer_app_password: process.env.NODEMAILER_APP_PASSWORD as StringValue,
   nodemailer_service: process.env.NODEMAILER_SERVICE as StringValue,
   nodemailer_smtp_host: process.env.SMTP_HOST,
   nodemailer_smtp_port: process.env.SMTP_PORT,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET as StringValue,
 };
