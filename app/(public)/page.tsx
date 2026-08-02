01e22c2e9104d1428f6d80a8c9149fad38b5dbda
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-02 13:24:31 +0600

    chor: home page make async

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { getAllPropertyDetails } from "@/lib/actions/property.actions";
+
+export default async function Home() {
+  const allProperties = await getAllPropertyDetails({
+    detailsAction: "categories",
+  });
+  console.log(allProperties);
   return <div>hello rent nest</div>;
 }
