e70c033b16dd29e90e5d6c71ba2ef73e43f74e02
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-02 15:26:21 +0600

    refactor: remove profile schema and marge with user

--- schemas/user.schema.ts
+++ schemas/user.schema.ts
@@ -32,6 +32,54 @@ export const AdminUserSchema = UserSchema.extend({
   status: UserStatusEnum.optional(),
 });
 
+export const ProfileSchema = z.object({
+  profileImage: z.string().optional(),
+  bio: z.string().max(1000).optional(),
+  birthdate: z.coerce.date().optional(),
+});
+
+export const ProfileUpdateSchema = z.object({
+  profileImage: z.string().optional(),
+
+  bio: z.string().max(1000).optional(),
+
+  birthdate: z.coerce.date().optional(),
+});
+
+export const SocialPlatformSchema = z.enum([
+  "GITHUB",
+  "LINKEDIN",
+  "FACEBOOK",
+  "TWITTER",
+  "INSTAGRAM",
+  "YOUTUBE",
+  "DISCORD",
+  "TELEGRAM",
+  "WHATSAPP",
+  "WEBSITE",
+]);
+
+export const SocialProfileCreateSchema = z.object({
+  platform: SocialPlatformSchema,
+  url: z.url(),
+});
+
+export const SocialProfileUpdateSchema = z.object({
+  url: z.url(),
+});
+
+export type SocialProfileCreateInput = z.infer<
+  typeof SocialProfileCreateSchema
+>;
+
+export type SocialProfileUpdateInput = z.infer<
+  typeof SocialProfileUpdateSchema
+>;
+
+export type ProfileInputType = z.infer<typeof ProfileSchema>;
+
+export type ProfileUpdateInputType = z.infer<typeof ProfileUpdateSchema>;
+
 export type UserInputType = z.infer<typeof UserSchema>;
 export type UserUpdateInputType = z.infer<typeof UserUpdateSchema>;
 export type AdminUserInputType = z.infer<typeof AdminUserSchema>;
