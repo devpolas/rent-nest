f834c5fe89cddfd2f6c88534b119a8681bdc3a79
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-03 03:01:03 +0600

    chor: all query export in central place

---
+++ hooks/index.ts
@@ -0,0 +1,46 @@
+// Queries
+export * from "./property/queries/use-properties";
+export * from "./property/queries/use-property";
+export * from "./property/queries/use-property-details";
+export * from "./property/queries/use-property-images";
+
+// Property mutations
+export * from "./property/mutations/property/use-create-property";
+export * from "./property/mutations/property/use-update-property";
+export * from "./property/mutations/property/use-admin-update-property";
+export * from "./property/mutations/property/use-delete-property";
+
+// Details mutations
+export * from "./property/mutations/details/use-create-property-details";
+export * from "./property/mutations/details/use-update-property-details";
+export * from "./property/mutations/details/use-delete-property-details";
+
+// Image mutations
+export * from "./property/mutations/images/use-create-property-images";
+export * from "./property/mutations/images/use-set-property-thumbnail";
+export * from "./property/mutations/images/use-delete-property-image";
+
+// Location mutations
+export * from "./location/mutations/use-create-location";
+export * from "./location/mutations/use-update-location";
+export * from "./location/mutations/use-delete-location";
+
+// queries
+export * from "./user/query/use-me";
+export * from "./user/query/use-user";
+export * from "./user/query/use-users";
+
+// user mutations
+export * from "./user/mutations/user/use-update-me";
+export * from "./user/mutations/user/use-delete-me";
+export * from "./user/mutations/user/use-update-user";
+export * from "./user/mutations/user/use-delete-user";
+
+// profile
+export * from "./user/mutations/profile/use-create-profile";
+export * from "./user/mutations/profile/use-update-profile";
+
+// social profile
+export * from "./user/mutations/social-profile/use-create-social-profile";
+export * from "./user/mutations/social-profile/use-update-social-profile";
+export * from "./user/mutations/social-profile/use-delete-social-profile";
