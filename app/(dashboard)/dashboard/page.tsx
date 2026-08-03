5781d4f324a06c02d1d14667ed37fc0edf881e52
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-04 00:19:58 +0600

    chor: create dashboard layout and components with shandcn/ui blocks

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,37 @@
-export default function page() {
-  return <section></section>;
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+import data from "./data.json";
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+import { SiteHeader } from "@/components/dashboard/site-header";
+import { SectionCards } from "@/components/dashboard/section-cards";
+import { ChartAreaInteractive } from "@/components/dashboard/chart-area-interactive";
+import { DataTable } from "@/components/dashboard/data-table";
+
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant='inset' />
+      <SidebarInset>
+        <SiteHeader />
+        <div className='flex flex-col flex-1'>
+          <div className='@container/main flex flex-col flex-1 gap-2'>
+            <div className='flex flex-col gap-4 md:gap-6 py-4 md:py-6'>
+              <SectionCards />
+              <div className='px-4 lg:px-6'>
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
