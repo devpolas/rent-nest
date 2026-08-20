285f34e2625307506ed956f67645708fbc8c1300
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-20 11:51:20 +0600

    chor: able to back properties from properties details page

--- components/properties/properties-details.tsx
+++ components/properties/properties-details.tsx
@@ -3,8 +3,12 @@
 import Loading from "@/app/loading";
 import { useProperty, usePropertyReviews } from "@/hooks";
 import PropertyDetails from "../property/property-details";
+import { Button } from "../ui/button";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function PropertyDetail({ id }: { id: string }) {
+  const router = useRouter();
   const { data, isLoading } = useProperty(id);
   const { data: propertyReviews, isLoading: propertyReviewLoading } =
     usePropertyReviews(id);
@@ -27,7 +31,16 @@ export default function PropertyDetail({ id }: { id: string }) {
   const reviews = propertyReviews?.data?.reviews ?? [];
 
   return (
-    <section className='mx-auto p-4 container'>
+    <section className='space-y-4 mx-auto p-4 container'>
+      <Button
+        variant='ghost'
+        size='sm'
+        className='-ml-2'
+        onClick={() => router.back()}
+      >
+        <ArrowLeft className='mr-2 size-4' />
+        Back to Properties
+      </Button>
       <PropertyDetails property={property} reviews={reviews} />
     </section>
   );
