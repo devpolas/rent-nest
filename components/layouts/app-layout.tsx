4d28c2d90b94dba13b42cef00ec2a4c46a379dd0
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-08-20 12:12:16 +0600

    fix: background color issue

--- components/layouts/app-layout.tsx
+++ components/layouts/app-layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer/footer";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex flex-col bg-background min-h-screen'>
       <Navbar />
 
       <main className='flex-1'>{children}</main>
